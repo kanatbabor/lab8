4e36b2c04a33e6751ab89fe25bea536e890e6134
local user <user@mil8a-102a-06.mmcs.sfedu.ru>
2024-10-04 13:34:27 +0300

    количество дней и является ли год високосным

--- time.pas
+++ time.pas
@@ -1,7 +1,7 @@
 ﻿begin
   var year := ReadInteger('введите год:');
   if (year mod 4 = 0) and (year mod 100 = 0) and (year mod 400 <> 0) then
-    println('ваш год является високосным')
+    println('ваш год является високосным, в нем 366 дней!')
   else
-    println('ваш год не является високосным');
+    println('ваш год не является високосным, в нем 365 дней!');
 end.
