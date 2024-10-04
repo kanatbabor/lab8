0999a2f4de8534dad22112a965c4e9d5cbfc5f87
local user <user@mil8a-102a-06.mmcs.sfedu.ru>
2024-10-04 13:09:24 +0300

    программа выводит текст в зависимости от часа

--- hello.pas
+++ hello.pas
@@ -1,3 +1,11 @@
 ﻿begin
-  Println('Привет, мир!');
+  var A := ReadInteger('введите текущий час:');
+  if A in 4..10 then
+    println('Доброе утро, мир!')
+  else if A in 11..16 then
+    println('Доброй день, мир!')
+  else if A in 17..22 then
+    println('Добрый вечер, мир!')
+  else 
+    println('Доброй ночи, мир!')
 end.
