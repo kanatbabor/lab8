7593ad4749ffe7ad481d490616b53fda532ee54d
local user <user@mil8a-102a-06.mmcs.sfedu.ru>
2024-10-04 13:18:27 +0300

    теперь вы узнаете, является ли введенный вами год високосным

---
+++ time.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var year := ReadInteger('введите год:');
+  if (year mod 4 = 0) and (year mod 100 = 0) and (year mod 400 <> 0) then
+    println('ваш год является високосным')
+  else
+    println('ваш год не является високосным');
+end.
